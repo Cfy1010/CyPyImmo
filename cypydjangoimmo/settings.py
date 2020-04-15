8a54cad7c2a8c96215fe0e6ea9e76b608ae7a5cd
Cyril <cy_one2000@yahoo.fr>
2020-04-15 19:29:34 +0200

    pull datas from listening model & display listing in template

--- cypydjangoimmo/settings.py
+++ cypydjangoimmo/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
 ]
 
 MIDDLEWARE = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
